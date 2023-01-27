08fa7d2929a58aeeac1e9449e74fd385894aa7c4
Arif Iskandar <arif19iskandar@gmail.com>
2023-01-27 18:24:19 +0700

    Replace tab with space

--- tests/validate-password.test.js
+++ tests/validate-password.test.js
@@ -2,77 +2,77 @@ const validatePassword = require('../utils/validate-password')
 
 test('return false if empty password', () => {
   // Given
-	const password = ''
+const password = ''
 
-	// When
-	const expected = validatePassword(password)
+  // When
+  const expected = validatePassword(password)
 
-	// Then
-	expect(expected).toBeFalsy()
+  // Then
+  expect(expected).toBeFalsy()
 })
 
 test('return false for password without numbers', () => {
-	// Given
-	const password = 'fghdjasdasas'
+  // Given
+  const password = 'fghdjasdasas'
 
-	// When
-	const expected = validatePassword(password)
+  // When
+  const expected = validatePassword(password)
 
-	// Then
-	expect(expected).toBeFalsy()
+  // Then
+  expect(expected).toBeFalsy()
 })
 
 test('return false for password without letters', () => {
-	// Given
-	const password = '567831239123'
+  // Given
+  const password = '567831239123'
 
-	// When
-	const expected = validatePassword(password)
+  // When
+  const expected = validatePassword(password)
 
-	// Then
-	expect(expected).toBeFalsy()
+  // Then
+  expect(expected).toBeFalsy()
 })
 
 test('return false for password with letters and numbers has less then 8 characters', () => {
-	// Given
-	const password = 'abcd123'
+  // Given
+  const password = 'abcd123'
 
-	// When
-	const expected = validatePassword(password)
+  // When
+  const expected = validatePassword(password)
 
-	// Then
-	expect(expected).toBeFalsy()
+  // Then
+  expect(expected).toBeFalsy()
 })
 
 test('return true for password with letters and numbers has more then 8 characters', () => {
-	// Given
-	const password = 'abcd123asdad'
+  // Given
+  const password = 'abcd123asdad'
 
-	// When
-	const expected = validatePassword(password)
+  // When
+  const expected = validatePassword(password)
 
-	// Then
-	expect(expected).toBeTruthy()
+  // Then
+  expect(expected).toBeTruthy()
 })
 
 test('return true for password with uppercase letters and numbers has more then 8 characters', () => {
-	// Given
-	const password = 'ABSS2213123'
+  // Given
+  const password = 'ABSS2213123'
 
-	// When
-	const expected = validatePassword(password)
+  // When
+  const expected = validatePassword(password)
 
-	// Then
-	expect(expected).toBeTruthy()
+  // Then
+  expect(expected).toBeTruthy()
 })
 
 test('return true for password with uppercase letters, lowercase letters and numbers has more then 8 characters', () => {
-	// Given
-	const password = 'ABSS2213123sadas'
+  // Given
+  const password = 'ABSS2213123sadas'
 
-	// When
-	const expected = validatePassword(password)
+  // When
+  const expected = validatePassword(password)
 
-	// Then
-	expect(expected).toBeTruthy()
+  // Then
+  expect(expected).toBeTruthy()
 })
