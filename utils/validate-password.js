db1af7e50a09aca62c4698626c849affdd16c917
Arif Iskandar <arif19iskandar@gmail.com>
2023-01-27 18:15:27 +0700

    Adding simple unit testing

---
+++ utils/validate-password.js
@@ -0,0 +1,29 @@
+function validatePassword(password = '') {
+    // at least 8 character
+    const validLength = password.length >= 8;
+
+    // has at least one letter
+    let hasLetter = false
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    for (const letter of alphabets) {
+        if (password.toLowerCase().includes(letter)) {
+            hasLetter = true
+            break
+        }
+    }
+
+    // has at least one letter
+    let hasNumber = false
+    const numbers = '0123456789'
+    for (const number of numbers) {
+        if (password.includes(number)) {
+            hasNumber = true
+            break
+        }
+    }
+
+    const validPassword = validLength && hasLetter && hasNumber
+    return validPassword
+}
+
+module.exports = validatePassword
